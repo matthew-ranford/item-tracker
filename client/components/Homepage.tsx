99ab0d1ec900428d958145dce6f66149ea87c50d
Matthew Ranford <matt.ranford16@gmail.com>
2023-12-26 20:20:05 +1300

    Further styling changes to navbar, homepage component updated with image, button, headers & p tags

--- client/components/Homepage.tsx
+++ client/components/Homepage.tsx
@@ -5,7 +5,13 @@ function Homepage() {
     <>
       <section className="hero">
         <div className="main-left">
-          <h1 className="header-left">About</h1>
+          <h1 className="header-left">
+            Keep track of the items you've lent so you always know where they
+            are
+          </h1>
+          <p className="p-left">Always misplacing your items?</p>
+          <p className="p-left">Simpy add an item below to get started </p>
+          <button className="button-left">Add Item</button>
         </div>
         <div className="main-right">
           <h1 className="header-right">Sign Up</h1>
